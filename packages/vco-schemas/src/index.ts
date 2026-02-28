bf4ec75e57e17531cdeb71cb20f2fd2642d88136
imattau <imatt.au@protonmail.com>
2026-02-28 13:46:41 +1100

    feat(vco-schemas): update index exports for all 4 schema domains

--- packages/vco-schemas/src/index.ts
+++ packages/vco-schemas/src/index.ts
@@ -1,4 +1,28 @@
+// Core schemas
 export * from "./post.js";
 export * from "./profile.js";
 export * from "./manifest.js";
 export * from "./assembly.js";
+
+// Social
+export * from "./social/reaction.js";
+export * from "./social/reply.js";
+export * from "./social/follow.js";
+export * from "./social/tombstone.js";
+export * from "./social/thread.js";
+
+// Marketplace
+export * from "./marketplace/listing.js";
+export * from "./marketplace/offer.js";
+export * from "./marketplace/receipt.js";
+
+// Files
+export * from "./files/file-descriptor.js";
+export * from "./files/directory.js";
+
+// Coordination
+export * from "./coordination/poll.js";
+export * from "./coordination/vote.js";
+export * from "./coordination/event.js";
+export * from "./coordination/rsvp.js";
+export * from "./coordination/announcement.js";
